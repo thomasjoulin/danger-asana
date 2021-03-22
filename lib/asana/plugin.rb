7a87eb4ab7f07329862575625a89218bd820fa10
Thomas Joulin <thomas.joulin@viki.com>
2021-03-22 11:39:16 +0800

    Add DangerAsana plugin implementation

---
+++ lib/asana/plugin.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require "asana"
+
+module Danger
+  # Links Asana issues to a pull request.
+  # @example Check PR for the following Asana project keys and links them
+  #
+  #          asana.check()
+  #
+  # @see  thomasjoulin/danger-asana
+  # @tags asana
+  #
+  class DangerAsana < Plugin
+    def initialize(dangerfile)
+      super(dangerfile)
+
+      @client = Asana::Client.new do |c|
+        c.authentication :access_token, ENV["_ASANA_TOKEN"]
+        c.default_headers "asana-enable" => "new_user_task_lists"
+      end
+    end
+
+    # Checks PR for Asana IDs and links them
+    #
+    # @return [void]
+    #
+    def check
+      issues = find_asana_issues
+
+      messages = []
+
+      issues.each do |issue|
+        task = find_by_id(issue)
+        puts task
+        messages << "*[#{task.name}](#{task.permalink_url})*\n#{task.notes}\n"
+      end
+
+      markdown messages.join("\n")
+    end
+
+    private
+
+    def vcs_host
+      return gitlab if defined? @dangerfile.gitlab
+
+      return github
+    end
+
+    def find_asana_issues(search_title: true, search_commits: true, search_body: true)
+      regexp = Regexp.new(/\[#([^\]]+)\]/)
+
+      asana_issues = []
+
+      if search_title
+        vcs_host.pr_title.gsub(regexp) do |match|
+          asana_issues << Regexp.last_match(1)
+        end
+      end
+
+      if search_commits
+        git.commits.map do |commit|
+          commit.message.gsub(regexp) do |match|
+            asana_issues << Regexp.last_match(1)
+          end
+        end
+      end
+
+      if search_body
+        vcs_host.pr_body.gsub(regexp) do |match|
+          asana_issues << Regexp.last_match(1)
+        end
+      end
+
+      return asana_issues.uniq
+    end
+
+    def find_by_id(id)
+      @client.tasks.find_by_id(id)
+    end
+  end
+end
---
+++ spec/asana_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require File.expand_path("spec_helper", __dir__)
+
+module Danger
+  describe Danger::DangerAsana do
+    it "should be a plugin" do
+      expect(Danger::DangerAsana.new(nil)).to be_a Danger::Plugin
+    end
+
+    describe "with Dangerfile" do
+      before do
+        @dangerfile = testing_dangerfile
+        @asana = @dangerfile.asana
+      end
+
+      it "Finds Asana task ids in body and title" do
+        allow(@asana).to receive_message_chain("github.pr_title").and_return("[#1200084894659949] Fix tvOS bummper start hiccup")
+        allow(@asana).to receive_message_chain("github.pr_body").and_return("[#1200084894659941]")
+
+        allow(@asana).to receive_message_chain("find_by_id.name").and_return("task 1", "task 2")
+        allow(@asana).to receive_message_chain("find_by_id.notes").and_return("description 1", "description 2")
+        allow(@asana).to receive_message_chain("find_by_id.permalink_url").and_return("https://app.asana.com/0/1", "https://app.asana.com/0/2")
+
+        @asana.check
+
+        expect(@dangerfile.status_report[:markdowns].first.message).to eq(%(*[task 1](https://app.asana.com/0/1)*
+description 1
+
+*[task 2](https://app.asana.com/0/2)*
+description 2
+))
+      end
+    end
+  end
+end
